685973eb587d49737d2ec0012505368ad1952c64
Ryan <apple@localhost.(none)>
2013-02-17 22:30:44 +0800

    change file

--- algs/lcs.py
+++ algs/lcs.py
@@ -10,28 +10,22 @@ def lcs(s1, s2):
 			arr.append(0)
 		tmp.append(arr)
 	
+	maxLen = 0
+	ind_i = 0
+	ind_j = 0
 	for i in range(1,len1):
 		for j in range(1,len2):
 			if s1[i] == s2[j]:
 				tmp[i][j] = tmp[i-1][j-1] + 1
-
-	maxLen = 0
-	ind_i = 0
-	ind_j = 0
-	for i in range(len1):
-		tmp_str = ''
-		for j in range(len2):
-			tmp_str += str(tmp[i][j]) + ' '
 			if tmp[i][j] > maxLen:
 				maxLen = tmp[i][j]
 				ind_i = i
 				ind_j = j
-		print tmp_str
-	
+
 	print "result: " + str(maxLen) + "--(" + str(ind_i-maxLen+1) + "," + str(ind_j-maxLen+1) + ")" + ":" + s1[ind_i-maxLen+1:ind_i+1]
 
 if __name__ == "__main__":
-	s1 = 'ADEDSDESD'
-	s2 = 'DEIELNDSD'
+	s1 = 'ADEDSDEKDOSDSDOFIJLENDDDDDDWLFSDOFLED'
+	s2 = 'DEIDLOWEELNDDDDDDDDDDSDKLDSJFOWENLGJSHLDFJ'
 	print s1 + ":" + s2
 	lcs(s1, s2)
